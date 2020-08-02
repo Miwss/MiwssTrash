06766ff9fd9dc237a0cac8609a59dced6b41ff8f
Miwss <M1wsoff@gmail.com>
2020-08-02 11:21:05 +0300

    add oop

--- OOP/OOP/Program.cs
+++ OOP/OOP/Program.cs
@@ -2,13 +2,13 @@
 
 namespace OOP
 {
-    enum Models {AUDI, VOLVO, NISSAN, VOLKSWAGEN, PIRHSE, FORD}
+   // enum Models {AUDI, VOLVO, NISSAN, VOLKSWAGEN, PIRHSE, FORD}
     class Cars
     {
         public int wheels = 4;// Доступен во всех классах
         private float speed; //Доступен только в этом классе
         protected bool isWorking = true;//Доступен в классе в котором за делегирован и классах наследниках
-        public Models model;
+       // public Models model;
         public static int test = 0;
 
         public static void Test()
@@ -35,15 +35,14 @@ namespace OOP
         public Cars() { }// peregryzka konstyktora(kotorui nichego ne peredaet)(для того, что бы при создании обьекта не нужно было обязательно вводить все данные!)
     }
 
-    class Trucks : Cars
-    {
-        public int passengers;
+   // class Trucks : Cars
+   //     public int passengers;
 
-        public Trucks(int wheels, float speed, bool isWorking, int passengers) : base (wheels, speed, isWorking)
-        {
-            this.passengers = passengers;
-        }
-    }
+    //    public Trucks(int wheels, float speed, bool isWorking, int passengers) : base (wheels, speed, isWorking)
+     //   {
+     //       this.passengers = passengers;
+     //   }
+  //  }
 
     class Program
     {
@@ -53,12 +52,12 @@ namespace OOP
             //Cars.Test();
             //Console.WriteLine(Cars.test);
 
-            Trucks man = new Trucks(8, 130.5f, true, 3);
-            man.getVelues();
-            Console.WriteLine(man.passengers);
+          //  Trucks man = new Trucks(8, 130.5f, true, 3);
+         //   man.getVelues();
+         //   Console.WriteLine(man.passengers);
 
             Cars toyota = new Cars(4, 230.5f, true);
-            toyota.model = Models.FORD;
+           // toyota.model = Models.FORD;
            // Console.WriteLine(toyota.wheels);
 
            // toyota.setValues(230.5f, true);
@@ -66,7 +65,7 @@ namespace OOP
 
             Cars audi = new Cars();//Таким образом мы создаем объект класса и выделяем ему память
            // audi.wheels = 6;// В этой строке мы вызываем один из методов и можем присвоить любое значение, для какждого объекта может быть свое значение не зависимо от того, что установленно в классе, в классе установленно общае значение для всех.
-            audi.model = Models.AUDI;
+           // audi.model = Models.AUDI;
             Console.WriteLine(audi.wheels);
             audi.setValues(260.0f, false);// указываем параметры для объекта 
             audi.getVelues();// выводим параметры кторые указали выше
---
+++ practick oop/practick oop/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace practick_oop
+{
+    class Ninja
+    {
+        protected int level = 0;
+        protected string rank;
+
+        public void setRang(int level, string rank)
+        {
+            this.level = level;
+            this.rank = rank;
+        }
+        public void getRang()
+        {
+            Console.WriteLine("Уровень ниндзя: " + this.level + "\n" + "Текущее звание: " + this.rank);
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Ninja yunger = new Ninja();
+            yunger.setRang(1, "Ниндзя новичок");
+            yunger.getRang();
+           // yunger.level = 1;
+           // yunger.rank = "Ninja novichek";
+           //Console.WriteLine("Уровень ниндзя: " + yunger.level + "\n" + "Текуще звание: " + yunger.rank);
+        }
+    }
+}
