bac352be86014ee6b55dd505e50fc5acde5eefbe
Miwss <M1wsoff@gmail.com>
2020-09-14 06:48:17 +0300

    food

---
+++ EdaArtemiy/EdaArtemiy/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace EdaArtemiy
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random rnd = new Random();
+            string[] ChooseFood = { "Лапша", "Суши", "Пицца", "Грузия", "Бургер", "Домашняя еда", "Салат" };
+            int Food = rnd.Next(ChooseFood.Length);
+            Console.WriteLine("Сегодня кушаем: {0}", ChooseFood[Food]);
+        }
+    }
+}
